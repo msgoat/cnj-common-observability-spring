ee216f4320a910eeb62bfe3112bbfc6bc9b2a075
Michael Theis (msg) <michael.theis@msg.group>
2023-02-25 16:21:49 +0100

    patched version 1.0.0
    - condition of LoggingMdcAutoConfiguration on SecurityContextHolder is working now
    - added condition of LoggingMdcAutoConfiguration on property cnj.observability.logging.mdc.enabled

--- src/main/java/group/msg/at/cloud/common/observability/logging/mdc/LoggingMdcAutoConfiguration.java
+++ src/main/java/group/msg/at/cloud/common/observability/logging/mdc/LoggingMdcAutoConfiguration.java
@@ -1,7 +1,10 @@
 package group.msg.at.cloud.common.observability.logging.mdc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +13,11 @@ import org.springframework.context.annotation.Configuration;
  * Configuration for logging context information.
  */
 @Configuration
-@ConditionalOnClass(name = "org/springframework/security/core/context/SecurityContextHolder")
+@ConditionalOnClass(name = "org.springframework.security.core.context.SecurityContextHolder")
+@ConditionalOnProperty(name = "cnj.observability.logging.mdc.enabled", havingValue = "true", matchIfMissing = true)
 public class LoggingMdcAutoConfiguration {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Value("${cnj.observability.logging.mdc.enabled:true}")
     private boolean enabled;
@@ -21,6 +27,7 @@ public class LoggingMdcAutoConfiguration {
 
     @Bean
     public FilterRegistrationBean<LoggingMdcFilter> loggingMdcFilter() {
+        logger.info("*** CONFIG *** Adding logging MDC filter to application context with enabled [{}]", enabled);
         FilterRegistrationBean<LoggingMdcFilter> result = new FilterRegistrationBean<>();
         LoggingMdcFilter filter = new LoggingMdcFilter();
         filter.setEnabled(this.enabled);
